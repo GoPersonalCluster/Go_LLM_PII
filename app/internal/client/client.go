25b8f561f7ab652576827a2e4cbb3faf999a35a9
walter <walter-eidi@hotmail.com>
2026-07-11 15:44:09 -0300

    encontrado problemas ao receber respostas do chat, aparentemente é um problema de asincronismo

--- app/internal/client/client.go
+++ app/internal/client/client.go
@@ -62,13 +62,13 @@ func (c *Client) Chat(ctx context.Context, prompt string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-
 	req, err := http.NewRequestWithContext(
 		ctx,
 		http.MethodPost,
 		c.BaseURL+"/api/chat",
 		bytes.NewBuffer(body),
 	)
+
 	if err != nil {
 		println("Error creating request:", err)
 		return "", err
@@ -77,24 +77,47 @@ func (c *Client) Chat(ctx context.Context, prompt string) (string, error) {
 	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := c.HTTPClient.Do(req)
-	println("Response status:", resp.Status)
-	println("Response body:", resp.Body)
+
 	if err != nil {
 		return "", err
 	}
-	defer resp.Body.Close()
+	//defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
 
-		data, _ := io.ReadAll(resp.Body)
-		return "", fmt.Errorf(string(data))
+		data, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return "", err
+		}
+
+		fmt.Println(string(data))
+
+		var result ChatResponse
+		if err := json.Unmarshal(data, &result); err != nil {
+			return "", err
+		}
 	}
-
-	var result ChatResponse
-
-	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+	time.Sleep(2 * time.Second)
+	println("llm data 100")
+	data, err := io.ReadAll(resp.Body)
+	println("llm data")
+	println(json.Marshal(data))
+	if err != nil {
+		println("error")
 		return "", err
 	}
 
+	fmt.Println(string(data))
+
+	var result ChatResponse
+	err = json.Unmarshal(data, &result)
+	println(json.Marshal(data))
+
+	// var result ChatResponse
+	// if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+
+	// 	return result.Message.Content, err
+	// }
+	println("llm data")
 	return result.Message.Content, nil
 }
